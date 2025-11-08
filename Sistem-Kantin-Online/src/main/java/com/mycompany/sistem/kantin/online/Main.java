83b2ae4423daef479856e367016cb5e350182b61
Aryo Abi Putra <aryoabiputra@gmail.com>
2025-11-08 14:49:19 +0700

    Add files via upload

---
+++ Sistem-Kantin-Online/src/main/java/com/mycompany/sistem/kantin/online/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistem.kantin.online;
+
+import com.mycompany.sistem.kantin.online.Login;
+
+/**
+ *
+ * @author aryo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new Login().setVisible(true); // Panggil JFrame Login
+        });
+    }
+}
